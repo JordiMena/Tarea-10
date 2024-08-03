a5cc2c4709f1a3e4cc7c5d60fd041b6526b4b531
JordiMena <jjyordy78@gmail.com>
2024-08-03 19:58:38 -0400

    Add files via upload

---
+++ ControlDeInventario.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ControlDeInventario.Domain.Entities;
+using ControlDelInventario.Domain.Entities;
+
+namespace ControlDeInventario.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Agregar configuraciones adicionales aquí si es necesario
+        }
+    }
+}
---
+++ ControlDeInventario.Infrastructure/Repositories/ProductoRepository.cs
@@ -0,0 +1,47 @@
+using ControlDeInventario.Domain.Interfaces;
+using ControlDelInventario.Domain.Entities;
+
+namespace ControlDeInventario.Infrastructure.Repositories
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Producto> GetAll()
+        {
+            return _context.Productos.ToList();
+        }
+
+        public Producto GetById(int id)
+        {
+            return _context.Productos.Find(id);
+        }
+
+        public void Add(Producto producto)
+        {
+            _context.Productos.Add(producto);
+            _context.SaveChanges();
+        }
+
+        public void Update(Producto producto)
+        {
+            _context.Productos.Update(producto);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var producto = _context.Productos.Find(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
